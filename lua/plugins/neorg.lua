9a5378d6832f64e164d21ea6091eacb9199b5b26
Joe Hays <hays.joe@gmail.com>
2025-03-15 17:29:23 -0400

    adding keybingings for neorg note creation, list creation, etc...

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -13,12 +13,29 @@ return {
         load = {
           ["core.defaults"] = {},
           ["core.concealer"] = {},
+          ["core.qol.todo_items"] = {}, -- Enables todo lists
           ["core.dirman"] = {
             config = {
               workspaces = {
                 notes = "~/notes",
               },
             },
+          },
+          ["core.keybinds"] = {
+              config = {
+                  hook = function(keybinds)
+                      local leader = "<LocalLeader>"
+                      keybinds.map("norg", "n", leader .. "td", "<Plug>(neorg.qol.todo-items.todo.task-done)")
+                      keybinds.map("norg", "n", leader .. "tu", "<Plug>(neorg.qol.todo-items.todo.task-undone)")
+                      keybinds.map("norg", "n", leader .. "tp", "<Plug>(neorg.qol.todo-items.todo.task-pending)")
+                      keybinds.map("norg", "n", leader .. "th", "<Plug>(neorg.qol.todo-items.todo.task-on_hold)")
+                      keybinds.map("norg", "n", leader .. "tc", "<Plug>(neorg.qol.todo-items.todo.task-cancelled)")
+                      keybinds.map("norg", "n", leader .. "tr", "<Plug>(neorg.qol.todo-items.todo.task-recurring)")
+                      keybinds.map("norg", "n", leader .. "ti", "<Plug>(neorg.qol.todo-items.todo.task-important)")
+                      keybinds.map("norg", "n", "<C-Space>", "<Plug>(neorg.qol.todo-items.todo.task-cycle)")
+                      keybinds.map("norg", "n", leader .. "tR", "<Plug>(neorg.qol.todo-items.todo.task-cycle-reverse)") -- Custom binding for reverse cycle
+                  end
+              }
           },
         },
       })
