6b1c51e4d4364dedff5063e023305c1f83a110a2
Joe Hays <joseph.t.hays3.civ@us.navy.mil>
2025-05-01 13:34:04 -0400

    I'll be honest... i made these changes too long ago... Can't remember...

---
+++ lua/plugins/nvim-osc52.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ojroques/nvim-osc52",
+    config = function()
+      local osc52 = require("osc52")
+      vim.keymap.set("v", "<leader>y", osc52.copy_visual, { desc = "yank to system clipboard" })
+    end,
+  },
+}
