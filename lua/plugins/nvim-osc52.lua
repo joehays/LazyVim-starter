71d2cfe9004b9cecd21eefc65c97bbf28d5cecc5
joehays <hays.joe@gmail.com>
2025-09-30 17:07:21 -0400

    just removed some comments

--- lua/plugins/nvim-osc52.lua
+++ lua/plugins/nvim-osc52.lua
@@ -4,6 +4,8 @@ return {
     config = function()
       local osc52 = require("osc52")
       vim.keymap.set("v", "<leader>y", osc52.copy_visual, { desc = "yank to system clipboard" })
+      -- TODO: add this to the LazyVim menu
+      -- TODO: add cut_visual keybindings as well
     end,
   },
 }
