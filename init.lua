1311b963f69208fa98e68f85d928116985a12077
joehays <hays.joe@gmail.com>
2026-02-16 12:48:27 -0500

    snap

--- init.lua
+++ init.lua
@@ -7,6 +7,13 @@ vim.g.maplocalleader = "\\"
 
 -- specify fonts to use
 vim.o.guifont = "FiraCodeNerdFontMono-Regular:h12" -- Replace "Fira Code:h12" with your chosen font and size
+--vim.o.guifont = "FiraCodeNerdFont-Regular:h12"
+
+-- Set the NVIM_LISTEN_ADDRESS for the current session.
+-- This makes the integrated terminal aware of the nvim server socket.
+vim.g.serveraddr = "/tmp/nvim"
+vim.env.NVIM_LISTEN_ADDRESS = vim.g.serveraddr
+
 --vim.o.guifontwidth = auto -- Adjust font width automatically
 --vim.o.guifontheight = auto -- Adjust font height automatically
 
